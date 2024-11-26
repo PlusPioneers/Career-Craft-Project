edec3efda9877c5c304e9fbe28b366829f74bd1e
PlusPioneers <pluspioneers.1667@gmail.com>
2024-11-26 11:55:09 +0530

    Update server.js

--- careerCraft-backend/server.js
+++ careerCraft-backend/server.js
@@ -8,5 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', aiRoutes);
 
-const PORT = process.env.PORT || 5000; // Use the PORT variable or default to 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
